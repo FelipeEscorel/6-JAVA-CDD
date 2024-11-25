90bde8bf5b842e9983bc18895b72d9f8f548f9f8
Felipe Escorel <felipeescorel@hotmail.com>
2024-11-25 09:18:09 -0300

    Add files via upload

---
+++ Aula003/Exercicio001.java
@@ -0,0 +1,26 @@
+package Aula003;
+
+import java.util.Scanner;
+
+public class Exercicio001 {
+
+	public static void main(String[] args) {
+		int alunos, cont = 0;
+		double nota, notaTotal = 0, media;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Quantos alunos tem na sua sala?");
+		alunos = sc.nextInt();
+		System.out.println("\nDigite a nota dos alunos: ");
+		
+		while (cont < alunos) {
+			nota = sc.nextDouble();
+			notaTotal = notaTotal + nota;
+			++cont;
+		}		
+		
+		media = notaTotal / alunos;
+		System.out.printf("A média de todas as notas é %.2f.", media);
+	}
+
+}
---
+++ Aula003/Idade.java
@@ -0,0 +1,15 @@
+package Aula003;
+
+public class Idade {
+
+	public static void main(String[] args) {
+		int idade = 15;
+		
+		while (idade < 18) {
+			System.out.println(idade);
+			idade = idade + 1;			
+		}
+
+	}
+
+}
