0ced7aa49bfeffcb70f19372939fbc7343d91e6f
Felipe Escorel <felipeescorel@hotmail.com>
2024-11-25 09:23:31 -0300

    Update Exercicio001.java

--- Aula003/Exercicio001.java
+++ Aula003/Exercicio001.java
@@ -6,7 +6,7 @@ public class Exercicio001 {
 
 	public static void main(String[] args) {
 		int alunos, cont = 0;
-		double nota, notaTotal = 0, media;
+		double nota = 0, media;
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Quantos alunos tem na sua sala?");
@@ -14,12 +14,11 @@ public class Exercicio001 {
 		System.out.println("\nDigite as notas dos alunos: ");
 		
 		while (cont < alunos) {
-			nota = sc.nextDouble();
-			notaTotal = notaTotal + nota;
+			nota += sc.nextDouble();
 			++cont;
 		}		
 		
-		media = notaTotal / alunos;
+		media = nota / alunos;
 		System.out.printf("A média de todas as notas é %.2f.", media);
 	}
 
