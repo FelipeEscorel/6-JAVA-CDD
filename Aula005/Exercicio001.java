29eefa2825cfb31c86b4e5cbb2fa3d056a8e6df3
Felipe Escorel <felipeescorel@hotmail.com>
2024-12-04 10:00:51 -0800

    Add files via upload

---
+++ Aula005/Exercicio001.java
@@ -0,0 +1,12 @@
+package cddAula005;
+
+public class Exercicio001 {
+
+	public static void main(String[] args) {
+		String str = " texto para retirar espaços no início e fim ";
+		String strEdit = str.trim();
+		System.out.println(strEdit);
+
+	}
+
+}
