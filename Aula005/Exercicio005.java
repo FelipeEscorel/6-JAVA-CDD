29731203f99242523252f64349aa4cac4b16a8a2
Felipe Escorel <felipeescorel@hotmail.com>
2024-12-04 10:54:15 -0800

    Add files via upload

---
+++ Aula005/Exercicio005.java
@@ -0,0 +1,11 @@
+package Aula005;
+
+public class Exercicio005 {
+
+	public static void main(String[] args) {
+		String array[] = {"a", "vida", "é", "bela"};
+        String texto = String.join(" ", array); // Junta as palavras com um espaço entre elas
+        System.out.println(texto);		
+	}
+
+}
