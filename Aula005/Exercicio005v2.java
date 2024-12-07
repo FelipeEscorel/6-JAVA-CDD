c555861be54a1c47c30634ed1d8c7242f968d2cd
Felipe Escorel <felipeescorel@hotmail.com>
2024-12-07 07:45:31 -0300

    Update Exercicio005v2.java

--- Aula005/Exercicio005v2.java
+++ Aula005/Exercicio005v2.java
@@ -3,15 +3,10 @@ package Aula005;
 public class Exercicio005v2 {
     public static void main(String[] args) {
         String array[] = {"a", "vida", "é", "bela"};
-        StringBuilder texto = new StringBuilder();
 
         for (int i = 0; i < array.length; i++) {
-            texto.append(array[i]); // Adiciona cada palavra
-            if (i < array.length - 1) {
-                texto.append(" "); // Adiciona espaço entre as palavras, exceto após a última
-            }
+        	System.out.print(array[i].toUpperCase() + " ");
         }
 
-        System.out.println(texto);
     }
 }
